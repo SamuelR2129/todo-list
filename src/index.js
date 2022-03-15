e79802e9a08439d9de7b9b0816b1514fb28eda6a
sam <samuel.reedy@hotmail.com>
2022-03-15 22:05:22 +1100

    added form wrapper and more add-project styling

--- src/index.js
+++ src/index.js
@@ -1,42 +1,64 @@
 import "./style.scss";
 
-document.querySelector("#add-project-btn").addEventListener("click", () => {
-  const projectSection = document.querySelector("#project-section");
+//document.querySelector("#add-project-btn").addEventListener("click", () => {
+const projectSection = document.querySelector("#project-section");
 
-  const addProjectContainer = document.createElement("div");
-  addProjectContainer.id = "add-project-container";
-  projectSection.appendChild(addProjectContainer);
+const addProjectWrapper = document.createElement("div");
+addProjectWrapper.id = "add-project-wrapper";
+projectSection.appendChild(addProjectWrapper);
 
-  const addProjectTitle = document.createElement("div");
-  addProjectTitle.className = "add-project-child";
-  addProjectContainer.appendChild(addProjectTitle);
+const addProjectForm = document.createElement("form");
+addProjectWrapper.appendChild(addProjectForm);
 
-  const projectTitle = document.createElement("h3");
-  projectTitle.textContent = "Title:";
-  addProjectTitle.appendChild(projectTitle);
+const addProjectContainer = document.createElement("div");
+addProjectContainer.id = "add-project-container";
+addProjectForm.appendChild(addProjectContainer);
 
-  const titleInput = document.createElement("input");
-  titleInput.setAttribute("type", "text");
-  addProjectTitle.appendChild(titleInput);
+const addProjectTitle = document.createElement("div");
+addProjectTitle.className = "add-project-child";
+addProjectContainer.appendChild(addProjectTitle);
 
-  const addProjectDate = document.createElement("div");
-  addProjectDate.className = "add-project-child";
-  addProjectContainer.appendChild(addProjectDate);
+const projectTitle = document.createElement("h3");
+projectTitle.id = "project-title-text";
+projectTitle.textContent = "Title:";
+addProjectTitle.appendChild(projectTitle);
 
-  const projectDate = document.createElement("h3");
-  projectDate.textContent = "Date:";
-  addProjectDate.appendChild(projectDate);
+const titleInput = document.createElement("input");
+titleInput.setAttribute("type", "text");
+addProjectTitle.appendChild(titleInput);
 
-  const dateInput = document.createElement("input");
-  dateInput.setAttribute("type", "text");
-  dateInput.placeholder = "DD/MM/YYYY";
-  addProjectDate.appendChild(dateInput);
+document.querySelector("input").select();
 
-  const addProjectButton = document.createElement("div");
-  addProjectButton.className = "add-project-child";
-  addProjectContainer.appendChild(addProjectButton);
+const addProjectDate = document.createElement("div");
+addProjectDate.className = "add-project-child";
+addProjectContainer.appendChild(addProjectDate);
 
-  const projectButton = document.createElement("button");
-  projectButton.textContent = "Submit";
-  addProjectContainer.appendChild(projectButton);
+const projectDate = document.createElement("h3");
+projectDate.id = "project-date-text";
+projectDate.textContent = "Date:";
+addProjectDate.appendChild(projectDate);
+
+const dateInput = document.createElement("input");
+dateInput.setAttribute("type", "text");
+dateInput.placeholder = "DD/MM/YYYY";
+addProjectDate.appendChild(dateInput);
+
+const addProjectButton = document.createElement("div");
+addProjectButton.className = "add-project-child";
+addProjectButton.id = "add-project-cont-btn";
+addProjectContainer.appendChild(addProjectButton);
+
+const projectButton = document.createElement("button");
+projectButton.id = "project-submit-button";
+projectButton.setAttribute("type", "submit");
+projectButton.setAttribute("value", "Submit");
+projectButton.textContent = "Submit";
+addProjectButton.appendChild(projectButton);
+//});
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  const formData = new FormData(e.target);
+  console.log(formData);
+  // Now you can use formData.get('foo'), for example.
+  // Don't forget e.preventDefault() if you want to stop normal form .submission
 });
